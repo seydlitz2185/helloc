d5db0284407cc9246192ef96aa2ac93d1287af92
seydlitz2185 <y188428292905@163.com>
2022-03-07 21:52:11 +0800

    Update CSAPP_2_review_universal

--- C/CSAPP_2_review_2.64/CSAPP_2_review_universal/main.c
+++ C/CSAPP_2_review_2.64/CSAPP_2_review_universal/main.c
@@ -87,11 +87,36 @@ void copy_int(int val, void *buf, int maxbytes){
 }
 
 /*2.73*/
+/* 正溢返回INT_MAX,负溢返回INT_MIN*/
+
+
+int staturating_add(int a, int b){
+    /*遵守CSAPP第二章习题的位级整数编码规则*/
+    int w = sizeof(int)<<3;
+    int SA = a >> (w-1);
+    /*SF用于表示两个加数的符号是否相同，同号SF = 0，异号SF = -1*/
+    int SF = a>>(w-1) ^ b>>(w-1);
+    /*若两数异号，一定不溢出，OF最后&～SF，异号时SF = -1，这时OF必定为0;
+     若两数同号，将a+b的符号与任意一个加数的符号～SA异或，判断溢出情况
+     如果a+b和加数异号，则认为溢出,0F = -1,否则为0*/
+    int OF = ((a + b) >> (w - 1) ^ SA) & ~SF  ;
+    /*用默认掩码0x80000000异或～SA生成掩码，
+     如果两个加数是负数，～SA = 0，掩码不变，即INT_MIN，0x80000000
+     若两个加数是正数，～SA = 1，相当于把掩码按位取反，即INT_MAX,0x7FFFFFFF
+     对于任意位的int都成立
+     */
+    int mask = (OF<<(w-1)^~SA) & OF;
+    /*如果没有溢出，OF和掩码都为0，这时&～OF和｜mask不影响a+b结果
+     否则将返回掩码，即INT_MIN或INT_MAX*/
+    return ((a+b)&~OF)|mask;
+}
+int (*f)(int,int) = &staturating_add;
+
 int main(int argc, const char * argv[]) {
     
     assert(!any_odd_one(0xAAAAAAAA));
     assert(odd_ones(0x11011001));
-    printf("%x\n",leftmost_one(0x00000000));
+    printf("%x\n",leftmost_one(0x40000000));
     assert(int_size_is_32());
     assert(int_size_is_32_for_16bit());
     printf("%x\n",lower_one_mask(31));
@@ -101,5 +126,11 @@ int main(int argc, const char * argv[]) {
     void* buf;
     buf = (void*)malloc(100*sizeof(int));
     copy_int(100, buf, 1);
+    assert(-1 == staturating_add(0xFFFFFFFE, 0x00000001));
+    assert((0+INT_MAX)  == staturating_add(0, INT_MAX));
+    assert((INT_MIN+0)  == staturating_add(INT_MIN, 0));
+    assert((212323+42242)  == f(42242, 212323));
+    assert(INT_MIN == staturating_add(0x80000000, 0x80000000));
+    assert(INT_MAX == staturating_add(0x70000002, 0x7FFFFFFF));
     return 0;
 }
